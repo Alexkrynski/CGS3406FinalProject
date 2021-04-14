9b31685ced74a1e124b45544041e52fef4e57d1d
Alexkrynski <80799391+Alexkrynski@users.noreply.github.com>
2021-04-14 18:57:39 -0400

    Create taskClass.h

---
+++ taskClass.h
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+ class Task {
+    public:
+    string taskName;
+    string dueDate;
+    string progress;
+    
+    void itr(Task arr[]);
+
+};
