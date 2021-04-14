b77f27c233a87763a0ed6e0c1e74d66f4f05e9eb
Alexkrynski <80799391+Alexkrynski@users.noreply.github.com>
2021-04-14 19:54:23 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,34 +5,31 @@
 
 using namespace std;
 
-Task tasks[10]; //array of task objects, max of 10 in application
+
+bool running = true;//for whole app
 
 
-
-
+Task taskArr[10]; //array of task objects, max of 10 in application
 int g_Tnum = 0;//global variable to control what task we're on
-
-
-
-
-
-
-bool running = true;
-int optFunc; //user picks between Big options
-int optTask; //navigates task management
+int optFunc; //user picks between 3 functions of app
+int optTask; //navigates 3 options of task management
 int cont; //continue/main menu
 int editClassNum;//progress function var
-int editProgNum;
+int editProgNum;//progress function var
 
 
 
 int main() {
     
+    taskArr[0].taskName = "test";
+taskArr[0].dueDate = "test date";
+    
     while(running) {
         
-       cout<<" -------------------------------------------------------------------------- " << endl;
-       cout<<"| 1. for Task management. 2. To Use Calculator 3. for [future function]|" << endl;
-       cout<<" -------------------------------------------------------------------------- " << endl;
+       cout<<"-------------------------------------------------------------------------- " << endl;
+       cout<<"|  Hi, I'm [name], your digital assistant. Press...                      | " << endl;
+       cout<<"| 1. for Task management. 2. To Use Calculator 3. for [future function]  | " << endl;
+       cout<<"-------------------------------------------------------------------------- " << endl;
        
        
        cin>>optFunc;
@@ -46,40 +43,36 @@ int main() {
                 
                     if(optTask == 1) { //new task
                         cin.ignore(); //clears buffer
-                        cout<<"Task name: ";
-                        getline(cin, tasks[g_Tnum].taskName);
+                        cout<<"New task name: ";
+                        getline(cin, taskArr[g_Tnum].taskName);
                         
                         cout<<"Due Date: ";
-                        getline(cin, tasks[g_Tnum].dueDate);
-                        cout<< "New task '" << tasks[g_Tnum].taskName << "' added sucessfully!";
+                        getline(cin, taskArr[g_Tnum].dueDate);
+                        cout<< "New task '" << taskArr[g_Tnum].taskName << "' added sucessfully!";
                         g_Tnum++; //to store next task in next index 
                         
                         
                     }
                     if(optTask == 2) //iterate current tasks
                     { 
-                         tasks[10].itr(tasks);
+                         taskArr[10].itr(taskArr);
                     }
                     if(optTask == 3) {
-                         tasks[10].itr(tasks);
+                         taskArr[10].itr(taskArr);
                         
-                        cout<<"Which task would you like to edit? \nTask: ";
+                        cout<<"\nWhich task would you like to edit? \nTask: ";
                         cin>>editClassNum;
-                        cout<< "Press 1. for not started, 2. for in progress, 3. for finished: ";
+                        cout<< "\nPress 1. for not started, 2. for in progress, 3. for finished: ";
                         cin>>editProgNum;
                         
-                        if(editProgNum == 1) 
-                            tasks[editClassNum - 1].progress = " not started";
-                        else if(editProgNum == 2)
-                            tasks[editClassNum - 1].progress = " in-progress";
-                        else if(editProgNum == 3)
-                            tasks[editClassNum - 1].progress = " finished";
+                        taskArr[10].progressChange(taskArr,editClassNum,editProgNum);
                         
                         
                     }
                 
-                    
+                cout<<"\n**********************************************************************";    
                 cout<<"\nPress '1' to continue in Task Manager or '2' to go to the main menu ";
+                cout<<"\n**********************************************************************"<< endl;
                 cin>>cont;
                     if (cont == 1) 
                         optFunc == 1;
@@ -93,10 +86,12 @@ int main() {
        while(optFunc == 2) 
        {
            cout<<"\n********************************************************************";
-           cout<<"\n[placeholder text for function 2 (calculator)]";
+           cout<<"\n[placeholder text for function 2]";
            cout<<"\n********************************************************************" << endl;
             
-           cout<<"\nPress '1' to continue in Calculator or '2' to go to the main menu ";
+           cout<<"\n**********************************************************************";    
+           cout<<"\nPress '1' to continue in Task Manager or '2' to go to the main menu ";
+           cout<<"\n**********************************************************************"<< endl;
               cin>>cont;
                   if (cont == 1) 
                       optFunc == 2;
@@ -114,14 +109,19 @@ int main() {
            
            
            
-           cout<<"\nPress '1' to continue in [FUNCTION 3] or '2' to go to the main menu ";
+           cout<<"\n**********************************************************************";    
+           cout<<"\nPress '1' to continue in Task Manager or '2' to go to the main menu ";
+           cout<<"\n**********************************************************************" << endl;
               cin>>cont;
                   if (cont == 1) 
                       optFunc == 3;
                   else
                       break;
-           
-       }   
-    }    
+       }
+    }
     return 0;
 }
+
+
+
+
