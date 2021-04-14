c83eb8f21531036d84fd63fa5df1208fd50c56e2
Alexkrynski <80799391+Alexkrynski@users.noreply.github.com>
2021-04-14 18:57:08 -0400

    Update taskClass.cpp

--- taskClass.cpp
+++ taskClass.cpp
@@ -1,25 +1,39 @@
 #include<iostream>
 #include<string>
+#include"taskClass.h"
 
-using namespace std;
 
-
-class Task {
-    public:
-    //task name
-    string taskName;
-    //due date
-    string dueDate;
-    //progress
-    string notStart, inProgress, finished;
-    
-    void setName(string name) {
-        taskName = name;
+void Task::itr(Task arr[]) //prints out task list based on how many there are 
+{
+    int responseSum = 0; // var to determine how many tasks there are
+    for(int i = 0; i < 9; i++) 
+    {
+        if (arr[i].taskName.length() > 0) 
+            responseSum = responseSum + 1;
     }
     
-    void setDate(string date) {
-        dueDate = date;
+        if(responseSum == 0)
+            cout<<"No tasks" << endl;
+        else
+        {
+            for(int j = 0; j < responseSum; j++) 
+                cout<< j + 1 << ". '" << arr[j].taskName << "'. due: " << arr[j].dueDate << ". Status: " << arr[j].progress << endl; 
+        }
         
-    }
-    //finished tasks
-};
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
