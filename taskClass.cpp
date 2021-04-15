459d74c86e579e6332eef33506d8fb8b4de5d73b
Alexkrynski <80799391+Alexkrynski@users.noreply.github.com>
2021-04-15 10:19:35 -0400

    Update taskClass.cpp
    new setter function

--- taskClass.cpp
+++ taskClass.cpp
@@ -3,6 +3,14 @@
 #include"taskClass.h"
 
 
+
+void Task::taskSetter(Task arr[], string name, string date, int g_Tnum) // name&date setter
+{
+    arr[g_Tnum].taskName = name;
+    arr[g_Tnum].dueDate = date;
+    cout<< "New task '" << arr[g_Tnum].taskName << "' added sucessfully!";
+    g_Tnum++;
+}
 void Task::itr(Task arr[]) //prints out task list based on how many there are 
 {
     int responseSum = 0; // var to determine how many tasks there are
@@ -16,12 +24,28 @@ void Task::itr(Task arr[]) //prints out task list based on how many there are
             cout<<"No tasks" << endl;
         else
         {
-            for(int j = 0; j < responseSum; j++) 
-                cout<< j + 1 << ". '" << arr[j].taskName << "'. due: " << arr[j].dueDate << ". Status: " << arr[j].progress << endl; 
+            cout<<"\n---------------------------";
+            cout<<"\nTasks";
+            cout<<"\n---------------------------" << endl;
+            for(int j = 0; j < responseSum; j++) //loop iterates task arr based on how many are there, calculated above 
+                cout<< j + 1 << ". '" << arr[j].taskName << "'. DUE: " << arr[j].dueDate << ". STATUS: " << arr[j].progress << endl; 
         }
         
 }
+void Task::progressChange(Task arr[], int editClassNum, int editProgNum ) 
+{ //self-explanatory
+    
+    if(editProgNum == 1) 
+        arr[editClassNum - 1].progress = "not started";
+    else if(editProgNum == 2)
+        arr[editClassNum - 1].progress = "in-progress";
+    else if(editProgNum == 3)
+        arr[editClassNum - 1].progress = "finished";
+
+    cout<< "\nProgress changed to: '" << arr[editClassNum - 1].progress <<  "' for '" << arr[editClassNum-1].taskName << "'" << endl;
+}
  
+
 
 
 
