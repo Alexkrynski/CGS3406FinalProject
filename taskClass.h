a12ced9fc828dfe7e6862a6c4009c8f4a90946a2
Alexkrynski <80799391+Alexkrynski@users.noreply.github.com>
2021-04-15 10:20:20 -0400

    Update taskClass.h
    updated class method prototypes (progressChange & taskSetter)

--- taskClass.h
+++ taskClass.h
@@ -4,10 +4,15 @@
 using namespace std;
  class Task {
     public:
+    //class vars
     string taskName;
     string dueDate;
     string progress;
     
+    //class method prototypes
     void itr(Task arr[]);
-
+    void progressChange(Task arr[], int editClassNum, int editProgNum );
+    void taskSetter(Task arr[], string name, string date, int g_num);
 };
+
+
