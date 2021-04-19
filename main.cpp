16d569869647b252d38e0d95d80e4a9f854179a3
galenolds32 <galenolds95@hotmail.com>
2021-04-19 01:57:42 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<string>
-
+#include<math.h>
 #include"taskClass.h"
 
 using namespace std;
@@ -80,11 +80,108 @@ taskArr[0].dueDate = "test date";
        
        while(optFunc == 2) 
        {
+           float a,b,PI;
+           int c;
            cout<<"\n********************************************************************";
            cout<<"\nWelcome to the Scientific Calculator";
            cout<<"\n********************************************************************" << endl;
+                cout<<"Operations\t"<<"\tTrigonometric Functions"<<"\t\tLogarithmic Functions\n";
+                cout<<"*****************************************************************\n";
+                cout<<"1 - Division\t\t"<<"7 - Sin\t\t"<<"\t\t13 - Log"<<endl;
+                cout<<"2 - Multiplication\t"<<"8 - Cos\t\t"<<"\t\t14 - Log Base 10"<<endl;
+                cout<<"3 - Subtract"<<"\t\t9 - Tan\t\t"<<endl;
+                cout<<"4 - Addition\t\t"<<"10 - Inverse Sin"<<endl;
+                cout<<"5 - Calculate Exponent"<<"11 - Inverse of Cos"<<endl;
+                cout<<"6 - Square Root"<<"12 - Inverse of Tan"<<endl;
+                cout<<"Enter the function that you want to execute: ";
+                cin>>c;
+                    PI=3.14159265358979323;
+                switch(c){
+                        case 1:
+                            cout<<"Enter the 1st number :";
+                            cin>>a;
+                            cout<<"Enter the 2nd number :";
+                            cin>>b;
+                            cout<<"Division = "<<a/b<<endl;
+                        break;
+                        case 2:
+                            cout<<"Enter the 1st number :";
+                            cin>>a;
+                            cout<<"Enter the 2nd number :";
+                            cin>>b;
+                            cout<<"Multiplication = "<<a*b<<endl;
+                        break;
+                        case 3:
+                            cout<<"Enter the 1st number :";
+                            cin>>a;
+                            cout<<"Enter the 2nd number :";
+                            cin>>b;
+                            cout<<"Subtraction = "<<a-b<<endl;
+                        break;
+                        case 4:
+                            cout<<"Enter the 1st number :";
+                            cin>>a;
+                            cout<<"Enter the 2nd number :";
+                            cin>>b;
+                            cout<<"Addition = "<<a+b<<endl;
+                        break;
+                        case 5:
+                            cout<<"Enter the base :";
+                            cin>>a;
+                            cout<<"Enter the POWER :";
+                            cin>>b;
+                            cout<<"Exponent = "<<pow(a,b)<<endl;
+                        break;
+                        case 6:
+                            cout<<"Enter a number to find the sqrt :";
+                            cin>>a;
+                            cout<<"Square Root = "<<sqrt(a)<<endl;
+                        break;
+                        case 7:
+                            cout<<"Enter a Number :";
+                            cin>>a;
+                            cout<<"Sin = "<<sin(a)<<endl;
+                        break;
+                        case 8:
+                            cout<<"Enter a number :";
+                            cin>>a;
+                            cout<<"Cos = "<<cos(a)<<endl;
+                        break;
+                        case 9:
+                            cout<<"Enter a number :";
+                            cin>>a;
+                            cout<<"Tan = "<<tan(a)<<endl;
+                        break;
+                        case 10:
+                            cout<<"Enter a number to find the Inverse of Sine :";
+                            cin>>a;
+                            cout<<"Inverse of Sin = "<<asin(a)*180.0/PI<<endl;
+                        break;
+                        case 11:
+                            cout<<"Enter a number to find the Inverse of Cosine :";
+                            cin>>a;
+                            cout<<"Inverse of Cos = "<<acos(a)*180.0/PI<<endl;
+                        break;
+                        case 12:
+                            cout<<"Enter a number to find the Inverse of Tangent :";
+                            cin>>a;
+                            cout<<"Inverse of Tan = "<<atan(a)*180.0/PI<<endl;
+                        break;
+                        case 13:
+                            cout<<"Enter a number to find Logarithm :";
+                            cin>>a;
+                            cout<<"Log = "<<log(a)<<endl;
+                        break;
+                        case 14:
+                            cout<<"Enter a nubmber to find the Logarithm Base 10 :";
+                            cin>>a;
+                            cout<<"Log Base 10 ="<<log10(a)<<endl;
+                        break;
+                        default:
+                            cout<<"Wrong Input"<<endl;
+                            
 
-                
+                }
 
            cout<<"\n**********************************************************************";    
            cout<<"\nPress '1' to continue in Task Manager or '2' to go to the main menu ";
